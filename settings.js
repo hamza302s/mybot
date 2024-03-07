54aeb7a601b0d1894c5c4f2bbd523e15c8175b3b
hamza302s <158991044+hamza302s@users.noreply.github.com>
2024-03-07 19:59:19 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'HAMZA Bot MD V11' //ur bot name
+global.ownernumber = '923229390827' //ur owner number
+global.ownername = '🦄드림 가이 HAMZA' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
+global.packname = "HAMZA AMIN"
+global.author = "🦄드림 가이 Xeon\n\n+923229390827"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["923229390827"] // Premium User
 global.hituet = 0
 
 //bot sett
